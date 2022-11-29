c68553c8ff6ff5bca75fa069da373f39219cf90e
yira <dladlfk1@gmail.com>
2022-11-29 20:41:31 +0900

    touch slider

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,109 +2,43 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleRight, faAngleLeft } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-const OverFlowBox = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  position: relative;
+const Carousel = styled(motion.div)`
+  cursor: grab;
   overflow: hidden;
 `;
-const Container1 = styled.div`
-  width: 340px;
-  height: 80%;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  background-color: green;
-  border-radius: 25px;
-  padding: 10px;
-  position: absolute;
-  z-index: 3;
-  box-shadow: 14px -5px 35px -21px rgba(0, 0, 0, 0.66);
-  left: 35%;
-`;
-const Container2 = styled.div`
-  width: 340px;
-  height: 80%;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  border-radius: 25px;
-  padding: 10px;
-  position: absolute;
-  left: 45%;
-  background-color: yellow;
-  z-index: 2;
-  scale: 0.9;
-  box-shadow: 14px -5px 35px -21px rgba(0, 0, 0, 0.66);
-`;
-const Container3 = styled.div`
-  width: 340px;
-  height: 80%;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  border-radius: 25px;
-  padding: 10px;
-  position: absolute;
-  background-color: green;
-  z-index: 1;
-  left: 55%;
-  scale: 0.8;
-  box-shadow: 14px -5px 35px -21px rgba(0, 0, 0, 0.66);
-`;
-const LBtnBox = styled.div`
-  position: absolute;
-  background-color: white;
-  z-index: 3;
-  left: 0;
-  top: 50%;
-  width: 40px;
-  height: 40px;
-  font-size: 2.3rem;
+
+const InnerCarousel = styled(motion.div)`
   display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
+  background-color: lightblue;
 `;
 
-const LayOutBox = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  position: relative;
-  overflow: hidden;
-`;
-const RBtnBox = styled.div`
-  position: absolute;
+const Item = styled(motion.div)`
+  min-height: 30rem;
+  min-width: 30rem;
+  padding: 40px;
+  border-radius: 2rem;
   background-color: white;
-  z-index: 3;
-  right: 0;
-  width: 40px;
-  height: 40px;
-  font-size: 2.3rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
 `;
-
 const Card = () => {
+  const [boardWidth, setBoardWidth] = useState(0);
+  const carousel = useRef<HTMLDivElement>(null);
   const array = [1, 2, 3, 4, 5, 6, 7];
-  const offset = 3;
-  const [index, setIndex] = useState(1);
+
+  useEffect(() => {
+    setBoardWidth(
+      carousel.current?.scrollWidth! - carousel.current?.offsetWidth!
+    );
+  });
   return (
-    <OverFlowBox>
-      <LBtnBox onClick={() => setIndex((prev) => prev - 1)}>
-        <FontAwesomeIcon icon={faAngleLeft} />
-      </LBtnBox>
-      <LayOutBox>
-        <Container1 />
-        <Container2 />
-        <Container3 />
-      </LayOutBox>
-      <RBtnBox onClick={() => setIndex((prev) => prev + 1)}>
-        <FontAwesomeIcon icon={faAngleRight} />
-      </RBtnBox>
-    </OverFlowBox>
+    <Carousel ref={carousel}>
+      <InnerCarousel drag="x" dragConstraints={{ right: 0, left: -boardWidth }}>
+        {array.map((i) => (
+          <Item>Hello</Item>
+        ))}
+      </InnerCarousel>
+    </Carousel>
   );
 };
 
