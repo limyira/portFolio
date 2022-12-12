7cae12450d49c9fb7fa966f9925bd183f6a0f30d
yira <dladlfk1@gmail.com>
2022-12-12 21:02:46 +0900

    notion

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleRight, faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useRef, useState } from "react";
-
+import CalcMain from "../images/calcMain.png";
 const Carousel = styled(motion.div)`
   cursor: grab;
   background-color: inherit;
@@ -61,7 +61,7 @@ const Image = styled.div`
   width: 100;
   height: 100%;
   border-radius: 2rem;
-  background-image: url("https://images.unsplash.com/photo-1523712999610-f77fbcfc3843?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80");
+  background-image: url(${CalcMain});
   background-position: center;
   background-size: cover;
 `;
